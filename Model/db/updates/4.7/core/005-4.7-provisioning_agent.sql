16cfa4c4fbaaeab8aa8167d7cdff434962225acf
Maxime Calmels <maxime.calmels@gmail.com>
2013-03-26 15:21:30 +0100

    [Git] Renommage scripts SQL pour version 4.7

--- Model/db/updates/4.7/core/005-4.7-provisioning_agent.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE agent
-	ADD COLUMN validation_code varchar(100),
-	ADD COLUMN validation_code_expiration timestamp without time zone,
-	ADD COLUMN password character varying(255);	
--- Model/db/updates/4.7/core/008-4.7-core-#7563-Fix-deleted-agents.sql
+++
@@ -1,2 +0,0 @@
--- Remove unknow references to Agent from CategoryRoles
-DELETE FROM agent_category_roles WHERE agent_id NOT in (SELECT id FROM Agent)
