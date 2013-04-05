d18fe5be06da57a66b4fa49223bb42ac4092830a
Maxime Calmels <maxime.calmels@gmail.com>
2013-04-05 16:26:39 +0200

    [Fix][Booker] Fix Iframe resizing for IE8-9

--- BackOfficeNG/web-app/js/frontoffice/planning.js
+++ BackOfficeNG/web-app/js/frontoffice/planning.js
@@ -1,16 +1,14 @@
 (function(planning) {
   var yud = YAHOO.util.Dom
     , yue = YAHOO.util.Event
- 
+
   planning.resize = function(event) {
-    if (  planning.url.indexOf(event.origin) > -1
-       && event.data.height
-       ) {
+    if (planning.url.indexOf(event.origin) > -1 && event.data > 0) {
       var iframe = yud.get('booker')
-      iframe.style.height = event.data.height + 'px'
+      iframe.style.height = event.data + 'px'
     }
   }
- 
+
   yue.on(window, 'message', planning.resize)
- 
+
 })(zenexity.capdemat.tools.namespace('zenexity.capdemat.fong.Planning'))
