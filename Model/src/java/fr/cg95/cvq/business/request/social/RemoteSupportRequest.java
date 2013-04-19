b66cbe477be34f8b02da4dc9a0d9f2c720c9507f
Maxime Calmels <maxime.calmels@gmail.com>
2013-04-19 10:38:39 +0200

    [Evo][TS][RSR] evo #6567 Add account completion step

--- Model/src/java/fr/cg95/cvq/business/request/social/RemoteSupportRequest.java
+++ Model/src/java/fr/cg95/cvq/business/request/social/RemoteSupportRequest.java
@@ -46,6 +46,13 @@ public class RemoteSupportRequest extends Request implements Serializable {
         
           stepState = new HashMap<String, Object>(4);
           stepState.put("state", "uncomplete");
+          stepState.put("required", false);
+          stepState.put("errorMsg", null);
+          stepState.put("invalidFields", new ArrayList<String>());
+          getStepStates().put("homeFolder", stepState);
+        
+          stepState = new HashMap<String, Object>(4);
+          stepState.put("state", "unavailable");
           stepState.put("required", true);
           stepState.put("errorMsg", null);
           stepState.put("invalidFields", new ArrayList<String>());
@@ -272,6 +279,13 @@ public class RemoteSupportRequest extends Request implements Serializable {
         
           stepState = new HashMap<String, Object>(4);
           stepState.put("state", "uncomplete");
+          stepState.put("required", false);
+          stepState.put("errorMsg", null);
+          stepState.put("invalidFields", new ArrayList<String>());
+          clone.getStepStates().put("homeFolder", stepState);
+        
+          stepState = new HashMap<String, Object>(4);
+          stepState.put("state", "unavailable");
           stepState.put("required", true);
           stepState.put("errorMsg", null);
           stepState.put("invalidFields", new ArrayList<String>());
