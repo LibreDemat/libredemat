e2b68110d596305413451917e7794a4c8bd41fdc
Maxime Calmels <maxime.calmels@gmail.com>
2013-04-19 10:00:40 +0200

    [Evo][TS][ERRR] evo #6567 Add account completion step

--- Model/src/java/fr/cg95/cvq/business/request/election/ElectoralRollRegistrationRequest.java
+++ Model/src/java/fr/cg95/cvq/business/request/election/ElectoralRollRegistrationRequest.java
@@ -46,6 +46,13 @@ public class ElectoralRollRegistrationRequest extends Request implements Seriali
         
           stepState = new HashMap<String, Object>(4);
           stepState.put("state", "uncomplete");
+          stepState.put("required", false);
+          stepState.put("errorMsg", null);
+          stepState.put("invalidFields", new ArrayList<String>());
+          getStepStates().put("homeFolder", stepState);
+        
+          stepState = new HashMap<String, Object>(4);
+          stepState.put("state", "unavailable");
           stepState.put("required", true);
           stepState.put("errorMsg", null);
           stepState.put("invalidFields", new ArrayList<String>());
@@ -179,6 +186,13 @@ public class ElectoralRollRegistrationRequest extends Request implements Seriali
         
           stepState = new HashMap<String, Object>(4);
           stepState.put("state", "uncomplete");
+          stepState.put("required", false);
+          stepState.put("errorMsg", null);
+          stepState.put("invalidFields", new ArrayList<String>());
+          clone.getStepStates().put("homeFolder", stepState);
+        
+          stepState = new HashMap<String, Object>(4);
+          stepState.put("state", "unavailable");
           stepState.put("required", true);
           stepState.put("errorMsg", null);
           stepState.put("invalidFields", new ArrayList<String>());
