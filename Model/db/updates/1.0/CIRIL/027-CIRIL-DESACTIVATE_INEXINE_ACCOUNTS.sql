80b40d217f512054dcf11e9dfed5823f38ae402f
Maxime Calmels <maxime.calmels@gmail.com>
2015-01-26 15:48:55 +0100

    MC121 - Deactivate Inexine's agents accounts

---
+++ Model/db/updates/1.0/CIRIL/027-CIRIL-DESACTIVATE_INEXINE_ACCOUNTS.sql
@@ -0,0 +1,4 @@
+update agent set active = false where login like '%inexine%';
+update agent set active = false where login = 'sebastien.faure';
+update agent set active = false where login = 'taibi.traitant';
+update agent set active = false where login = 'taibi.admin';
