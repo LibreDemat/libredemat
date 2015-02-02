1841fd5fa6f93eae9a5193bef109c153f7aff0ff
Maxime Calmels <maxime.calmels@gmail.com>
2015-02-02 09:12:09 +0100

    MC-132 - Initialize home folders with default email as imported and not initialized

--- Model/db/updates/1.0/CIRIL/029-CIRIL-FLAG_IMPORTED_HOME_FOLDERS.sql
+++ Model/db/updates/1.0/CIRIL/029-CIRIL-FLAG_IMPORTED_HOME_FOLDERS.sql
@@ -1,2 +1,7 @@
 alter table home_folder add column is_imported_and_not_initialized bool;
 update home_folder set is_imported_and_not_initialized = false;
+update home_folder set is_imported_and_not_initialized = true
+    where id in (select individual_role.home_folder_id from adult, individual_role
+                 where adult.id = individual_role.owner_id
+                 and individual_role.role = 'HOME_FOLDER_RESPONSIBLE'
+                 and adult.email = 'Changer@LeMail.fr');
