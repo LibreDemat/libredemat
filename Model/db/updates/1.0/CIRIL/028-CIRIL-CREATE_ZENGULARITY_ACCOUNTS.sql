4aed6e7b522783fe9e04e178bda1e0ee191c78b4
Maxime Calmels <maxime.calmels@gmail.com>
2015-04-21 13:33:38 +0200

    Improve SQL migration script

--- Model/db/updates/1.0/CIRIL/028-CIRIL-CREATE_ZENGULARITY_ACCOUNTS.sql
+++ Model/db/updates/1.0/CIRIL/028-CIRIL-CREATE_ZENGULARITY_ACCOUNTS.sql
@@ -4,7 +4,7 @@ insert into agent_site_roles (agent_id, profile)
     values ((select id from agent where login = 'agent.zengularity'), 'AGENT');
 insert into user_security_rule (id, agent_id, profile)
     values (nextval('hibernate_sequence'), (select id from agent where login = 'agent.zengularity'), 'MANAGE');
-insert into agent_category_roles select id, (select id from agent where login = 'agent.zengularity'), 'MANAGER' from category;
+insert into agent_category_roles (category_id, agent_id, profile) select id, (select id from agent where login = 'agent.zengularity'), 'MANAGER' from category;
 
 insert into agent (id, active, email, first_name, last_name, login, preferences, validation_code, validation_code_expiration, password) 
     values (nextval('hibernate_sequence'), true, 'support-capdemat@zenexity.com', 'admin', 'zengularity', 'admin.zengularity', null, null, null, '$2a$10$OPsShQpOVhYRe7huUan9JuV9DmO1vM20y8rsZYIAqtcfBsZGul/QS');
