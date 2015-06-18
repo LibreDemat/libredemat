5fd5356a76e0d45cb6e869107ffc25f42386fb58
Maxime Calmels <maxime.calmels@gmail.com>
2015-06-18 17:48:50 +0200

    MC-177 - One SQL instruction at a time to better identify potential problems

--- Model/db/updates/1.1.1/CIRIL/001-TS_HSR.sql
+++ Model/db/updates/1.1.1/CIRIL/001-TS_HSR.sql
@@ -1,8 +1,20 @@
 alter table holiday_security_request 
-    add column animal_information varchar(255),
-    add column is_animal_owner bool,
-    add column is_security_company bool,
-    add column other_contact_duplicate_key bool,
-    add column security_company_address varchar(255),
-    add column security_company_name varchar(255),
+    add column animal_information varchar(255);
+
+alter table holiday_security_request 
+    add column is_animal_owner bool;
+
+alter table holiday_security_request 
+    add column is_security_company bool;
+
+alter table holiday_security_request 
+    add column other_contact_duplicate_key bool;
+
+alter table holiday_security_request 
+    add column security_company_address varchar(255);
+
+alter table holiday_security_request 
+    add column security_company_name varchar(255);
+
+alter table holiday_security_request 
     add column security_company_telephone varchar(10);
